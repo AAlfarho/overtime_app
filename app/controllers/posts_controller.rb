de6b4a806e54e805684b9da01f691ba8299a9529
aalfarho <st.alfaro@gmail.com>
2017-04-05 00:02:11 +0000

    implemented post creation, refacotre spec for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,37 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
+    
     def index
     end
+    
+    def new
+        @post = Post.new
+    end
+    
+    def create
+        @post = Post.new(post_params)
+        respond_to do |format|
+            if @post.save
+                format.html { redirect_to @post, notice: 'Post was successfully created.' }
+                format.json { render :show, status: :created, location: @post }
+            else
+                format.html { render :new }
+                format.json { render json: @post.errors, status: :unprocessable_entity }
+          end
+        end
+    end
+    
+    def show
+        #we dont need this as we already have it set on the before_action :set_post
+        #@post = Post.find(params[:id])
+    end
+    
+    private
+        def post_params
+            params.require(:post).permit(:date, :rationale)
+        end
+        
+        def set_post
+            @post = Post.find(params[:id])
+        end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,4 +12,22 @@ describe 'navigate' do
             expect(page).to have_content("Posts")
         end
     end
+    
+    describe "creation" do 
+        before do
+            visit new_post_path
+        end
+        
+        it 'can reach form' do 
+            expect(page.status_code).to eq(200)
+        end
+        
+        it "can be created from new page" do 
+            fill_in 'post[date]', with: Date.today
+            fill_in 'post[rationale]', with: 'Rationale set'
+            
+            click_on 'Save'
+            expect(page).to have_content("Rationale set")
+        end
+    end
 end
