cae0c7bd39de9a2f6c91cbea3807907c7560696e
AAlfarho <st.alfaro@gmail.com>
2017-04-07 16:56:35 -0400

    added nav var por adding posts

--- spec/features/static_spec.rb
+++ spec/features/static_spec.rb
@@ -6,5 +6,12 @@ describe 'navigate' do
       visit root_path
       expect(page.status_code).to eq(200)
     end
+    describe 'new tab' do
+      it 'can be reached from the beginning' do
+        visit root_path
+        click_link('new_post_from_nav')
+        expect(page.status_code).to eq(200)
+      end
+    end
   end
 end
