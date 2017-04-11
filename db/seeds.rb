f4bf410c4d113fee262dbd3dffa0dc4ba3f3bb37
AAlfarho <st.alfaro@gmail.com>
2017-04-11 19:48:30 -0400

    implemented enum for post status

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 @user = User.create(email: "test@test.com", password: "asdasd", password_confirmation: "asdasd", first_name: "andres", last_name: "alfaro")
+@amind_user = AdminUser.create(email: "admin@admin.com", password: "asdasd", password_confirmation: "asdasd", first_name: "andres el admin", last_name: "alfaro")
 @user.save
 100.times do |post|
     Post.create(date: Date.today, rationale: "Rationale n #{post}", user_id: @user.id)
