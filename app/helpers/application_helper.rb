e81132c16e93a9c22c7042d1ac7c22dd10bd07b7
AAlfarho <st.alfaro@gmail.com>
2017-04-11 19:21:28 -0400

    block access to admin dashboard to non admin users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
   def active?(path)
-    puts "im the path #{path}"
     'active' if current_page?(path)
   end
 end
--- spec/features/static_spec.rb
+++ spec/features/static_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  before do
+    @user = FactoryGirl.create(:user)
+    login_as(@user, :scope => :user)
+  end
   describe 'homepage' do
     it 'can be reached successfully' do
       visit root_path
