47de3fa330e1bf4ca3f0566e7e4bab9118f1786b
AAlfarho <st.alfaro@gmail.com>
2017-04-12 18:16:43 -0400

    label implementation depending on posts status

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,18 @@
 module PostsHelper
+  def status_label status
+    status_span_generator(status)
+  end
+
+  def status_span_generator status
+    label_class = ""
+    case status.to_sym
+      when :submitted
+        label_class = 'label-primary'
+      when :approved
+        label_class = 'label-success'
+      when :rejected
+        label_class = 'label-danger'
+    end
+    content_tag(:span, status.upcase, class: "label #{label_class}")
+  end
 end
