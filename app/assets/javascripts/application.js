a93e355c7af8bce96b8d53e4daadab1de4540ce7
AAlfarho <st.alfaro@gmail.com>
2017-04-11 22:12:24 -0400

    added pundit, updated nav bar, fixed css imports

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+
+
 //= require jquery
+// require jquery.turbolinks
+//= require jquery_ujs
 //= require bootstrap-sprockets
 //= require gritter
-//= require jquery_ujs
-//= require turbolinks
 //= require_tree .
